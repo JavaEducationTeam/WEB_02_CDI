5d1a183f9a95d20caa9d5536a26b5709b6589afb
krisztian666 <krisztian@mozilla.hu>
2017-11-22 02:41:56 +0100

    CDI Hero

---
+++ heroesofempires/webapplication/src/main/java/hu/javagladiator/app/heroesofempires/webapplication/HeroServiceCDI.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hu.javagladiator.app.heroesofempires.webapplication;
+
+import hu.javagladiators.app.heroesofempires.dataservice.fileio.HeroServiceImpl;
+
+
+/**
+ *
+ * @author krisztian
+ */
+//@javax.faces.bean.ApplicationScoped
+@javax.enterprise.context.ApplicationScoped
+public class HeroServiceCDI extends HeroServiceImpl{
+    
+}
